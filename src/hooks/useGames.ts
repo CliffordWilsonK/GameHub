e6a4eb9d716c4944c67c169b2409541857181275
wilsoncliffordkwame-oss <wilsoncliffordkwame>
2025-11-14 19:28:29 +0000

    Remove unused imports from useGames hook

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,9 +1,4 @@
-import { useEffect, useState } from "react";
-import apiClient from "@/services/api-client";
-import { CanceledError } from "axios";
 import useData from "./useData";
-
-
 export interface Platform{
     id: number;
     name: string;
