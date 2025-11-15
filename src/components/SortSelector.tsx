6813c181bbaddebff417de2714c7609dba5e4ac5
wilsoncliffordkwame-oss <wilsoncliffordkwame>
2025-11-15 21:04:44 +0000

    Enhance Sort Selector and Game Query: Integrate sortOrder state management and improve sorting options in SortSelector component.

--- src/components/SortSelector.tsx
+++ src/components/SortSelector.tsx
@@ -1,23 +1,43 @@
 import { Button, Menu, Portal } from "@chakra-ui/react";
 import { BsChevronDown } from "react-icons/bs";
 
-const SortSelector = () => {
+interface Props {
+  onSelectOrder: (sortOrder: string) => void;
+  sortOrder?: string;
+}
+
+const SortSelector = ({ onSelectOrder, sortOrder }: Props) => {
+  const sortOrders = [
+    { value: "", label: "Relevance" },
+    { value: "-added", label: "Date Added" },
+    { value: "-released", label: "Release Date" },
+    { value: "name", label: "Name" },
+    { value: "-metacritics", label: "Popularity" },
+    { value: "-rating", label: "Average Rating" },
+  ];
+  const currentSortOrder =
+    sortOrders.find((order) => order.value === sortOrder)?.label || "Relevance";
   return (
     <Menu.Root>
       <Menu.Trigger asChild>
         <Button variant="outline" size="sm">
-          Order By: Relevance <BsChevronDown />
+          Order By: {currentSortOrder} <BsChevronDown />
         </Button>
       </Menu.Trigger>
       <Portal>
         <Menu.Positioner>
           <Menu.Content>
-            <Menu.Item value="">Relevance</Menu.Item>
-            <Menu.Item value="">Date Added</Menu.Item>
-            <Menu.Item value="">Release Date</Menu.Item>
-            <Menu.Item value="">Name</Menu.Item>
-            <Menu.Item value="">Popularity</Menu.Item>
-            <Menu.Item value="">Average Rating</Menu.Item>
+            {sortOrders.map((order) => (
+              <Menu.Item
+                key={order.value}
+                value={order.value}
+                onClick={() => {
+                  onSelectOrder(order.value);
+                }}
+              >
+                {order.label}
+              </Menu.Item>
+            ))}
           </Menu.Content>
         </Menu.Positioner>
       </Portal>
--- src/services/getCroppedImages.ts
+++ src/services/getCroppedImages.ts
@@ -1,6 +1,7 @@
 
 
 const getCroppedImage = (url: string) => {
+    if (!url) return "https://placehold.co/600x400.png?text=Image+not+found";
     const target = "media/";
     const index = url.indexOf(target) + target.length
     return url.slice(0, index) + "crop/600/400/" + url.slice(index)
